8d506b9968d231980bd3ae4fd6ef7f9507d92bf6
XyHK-HUC <140715298+XyHK-HUC@users.noreply.github.com>
2024-08-02 04:50:03 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f24ea60c-fe03-4ac6-a0b0-96e1a7894ee3';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
